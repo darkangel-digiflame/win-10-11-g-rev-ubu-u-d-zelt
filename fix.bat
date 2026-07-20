b229e0f6e05983e45d259b62dbde46435ce1de29
ufuk cingay <cingay.ufuk@yaani.com>
2026-07-20 22:52:33 +0300

    Modify fix.bat to enhance output messages
    Updated echo statements and added email and phone number.

--- fix.bat
+++ fix.bat
@@ -1,6 +1,6 @@
-Echo " MS Windows 10/11 Görev çubuğu düzeltme-sıfırlama  programı Elektronik Müh Ufuk ÇINGAY  © Temmuz 2026 http://www.ufukcingay.net"
-Echo " Bilişim sektöründe 50'ci yıl!...!"
-echo off@
+Echo MS Windows 10/11 Görev çubuğu düzeltme-sıfırlama  programı Elektronik Müh Ufuk ÇINGAY  © Temmuz 2026 http://www.ufukcingay.net
+Echo http://www.ufukcingay.net ufukcingay@ufukcingay.net +90-0850-8071969
+@echo off
 DEL /F /S /Q /A "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\*"
 REG DELETE HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband /F
 taskkill /f /im explorer.exe
